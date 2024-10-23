5c97546c1ef96df474e35862bace9d89700c15bd
Min Zhengjie <minzhengjie@sjtu.edu.cn>
2024-10-23 11:55:15 +0800

    feat(archive): pre-commit

--- examples/engr151.py
+++ examples/engr151.py
@@ -5,14 +5,17 @@ from joint_teapot import Teapot
 
 
 class ENGR151Teapot(Teapot):
-    def archive_repos(self, regex: str) -> None:
+    def archive_repos(self, regex: str, dry_run: bool = False) -> None:
+        if dry_run:
+            print("Dry run enabled. No changes will be made to the repositories.")
         print(f"Archiving repos with name matching {regex}")
         for repo_name in self.gitea.get_all_repo_names():
             if re.match(regex, repo_name):
                 print(f"Archived {repo_name}")
-                self.gitea.repository_api.repo_edit(
-                    self.gitea.org_name, repo_name, body={"archived": True}
-                )
+                if not dry_run:
+                    self.gitea.repository_api.repo_edit(
+                        self.gitea.org_name, repo_name, body={"archived": True}
+                    )
             else:
                 print(f"Skipped {repo_name}")
 
@@ -27,12 +30,18 @@ if __name__ == "__main__":
     archive_parser.add_argument(
         "regex", type=str, help="The regex pattern to match repository names."
     )
+    archive_parser.add_argument(
+        "-d",
+        "--dry-run",
+        action="store_true",
+        help="Perform a dry run without making any changes.",
+    )
 
     args = parser.parse_args()
 
     teapot = ENGR151Teapot()
 
     if args.command == "archive-repos":
-        teapot.archive_repos(args.regex)
+        teapot.archive_repos(args.regex, args.dry_run)
     else:
         parser.print_help()
